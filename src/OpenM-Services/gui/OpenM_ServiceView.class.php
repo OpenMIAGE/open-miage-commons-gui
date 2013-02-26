008a11acd15c57b641751d8e1ffa308dcd01a8ff
gsaunier <gael.saunier@open-miage.fr>
2013-02-26 21:34:14 +0100

    initialize project

--- src/OpenM-Services/gui/OpenM_ServiceView.class.php
+++ src/OpenM-Services/gui/OpenM_ServiceView.class.php
@@ -7,7 +7,18 @@ Import::php("util.Properties");
 Import::php("OpenM-Services.gui.OpenM_ServiceViewException");
 
 /**
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @link http://www.open-miage.org
  * @author Gaël Saunier
  */
 abstract class OpenM_ServiceView {
@@ -24,7 +35,7 @@ abstract class OpenM_ServiceView {
     const LOG_PATH_PROPERTY = "OpenM_Log.path";
     const LOG_FILE_NAME = "OpenM_Log.file.name";
     const DEFAULT_FORM = "_default";
-    
+
     protected $configFile;
     protected $template_c;
     protected $ressources_dir;
@@ -46,10 +57,11 @@ abstract class OpenM_ServiceView {
     }
 
     public abstract function _default();
-    
-    public static function getClass(){
+
+    public static function getClass() {
         return get_called_class();
     }
+
 }
 
 ?>
--- src/OpenM-Services/gui/OpenM_ServiceViewSSO.class.php
+++ src/OpenM-Services/gui/OpenM_ServiceViewSSO.class.php
@@ -6,13 +6,24 @@ Import::php("util.http.OpenM_Header");
 
 /**
  *
- *
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @link http://www.open-miage.org
  * @author Gaël Saunier
  */
 abstract class OpenM_ServiceViewSSO extends OpenM_ServiceView {
 
     const SSO_CONFIG_FILE_PATH = "OpenM_SSO.client.config.path";
-    
+
     /**
      *
      * @var OpenM_SSOClientSession 
@@ -33,6 +44,7 @@ abstract class OpenM_ServiceViewSSO extends OpenM_ServiceView {
             throw new OpenM_ServiceViewException(self::SSO_CONFIG_FILE_PATH . " not defined in " . self::CONFIG_FILE_NAME);
         $this->manager = OpenM_SSOClientPoolSessionManager::fromFile($path);
     }
+
 }
 
 ?>
